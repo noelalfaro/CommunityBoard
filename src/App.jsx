6b55a6b758b4ca7f719282ff521ddd3e3de994d4
Noel <ndalfaro333@gmail.com>
2023-02-22 01:29:19 -0600

    committing  all files besides readme

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>First commit for the project</h1>
+  )
+}
+
+export default App
